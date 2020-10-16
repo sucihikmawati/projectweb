08fa7c464a49820d0a2cde83f62602392e76a824
sucihikmawati <sucihikmawati24@gmail.com>
2020-10-16 17:00:15 +0700

    my project

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,6 +7,17 @@ class Home extends BaseController
 		return view('welcome_message');
 	}
 
+	public function show()
+	{
+		$data['nama'] = "suci hikmawati";    //mengiirm data ke view
+		$data['npm'] = "1817051033";
+		$data['jurusan'] = "Ilmu Komputer";
+
+		echo view('mahasiswa/header', $data);
+		echo view('mahasiswa/index', $data); 
+		echo view('mahasiswa/footer', $data);
+
+	}
 	//--------------------------------------------------------------------
 
 }
---
+++ app/Views/Mahasiswa/index.php
@@ -0,0 +1,13 @@
+	<h1>Tabel Mahasiswa</h1>
+	<table border=1>
+		<tr> 
+			<td> nama </td>
+			<td> npm </td>
+			<td> jurusan</td>
+		</tr>
+		<tr>
+			<td> <?= $nama ?> </td>
+			<td> <?= $npm ?> </td>
+			<td> <?= $jurusan ?> </td>
+		</tr>
+	</table>
